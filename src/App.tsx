2ebdba92e443b664adeea55edf360032317f69ac
Brendan Murphy <brendan.murphy@ostusa.com>
2023-10-27 13:57:31 -0400

    Starter Code

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Nav } from './Nav';
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <div className='m-8'>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
