e9be0157d694908c60b4fbdaeb55b65a92252f96
Brendan Murphy <brendan.murphy@ostusa.com>
2023-10-29 16:42:23 -0400

    New Nav Placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Nav } from './Nav';
+import Nav from './Nav';
 import Home from './home page/Home';
 
 function App() {
   return (
     <>
       <Nav />
-      <div className='m-8'>
+      <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-4 '>
         <Home />
       </div>
     </>
--- src/shared/Card.tsx
+++ src/shared/Card.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Card({ title, description, img, alt }: Props) {
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-      <img className='w-full' src={img} alt={alt} />
+      <img className='w-full' src={img} alt={alt} loading='lazy' />
       <div className='px-6 py-4'>
         <div className='font-bold text-xl mb-2'>{title}</div>
         <p className='text-gray-700 text-base'>{description}</p>
