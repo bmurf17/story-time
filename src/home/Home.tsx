cc84d6b85bceb7c60b4cffe50a13bdd2689a3ba9
Brendan Murphy <brendan.murphy@ostusa.com>
2024-01-14 13:18:57 -0500

    Missing from last commit

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import Card from '../shared/Card';
 import HighlightCard from './_HighlightCard';
+import ProjectScroller from './_ProjectScroller';
 
 export default function Home() {
   return (
@@ -15,20 +15,8 @@ export default function Home() {
           />
         </Link>
       </div>
-      <div className='grid grid-flow-row auto-rows-max grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4'>
-        <Card
-          img='https://v1.tailwindcss.com/img/card-top.jpg'
-          alt='Sunset in the mountains'
-          description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.'
-          title='The Coldest Sunset'
-        />
-        <Card
-          img='https://v1.tailwindcss.com/img/card-top.jpg'
-          alt='Sunset in the mountains'
-          description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.'
-          title='The Coldest Sunset'
-        />
-      </div>
+
+      <ProjectScroller />
     </div>
   );
 }
